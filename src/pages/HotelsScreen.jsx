6eb3975ec44d6f77dca46119abc8051d856ef7d4
BrianFs04 <brayanflorezsanabria@gmail.com>
2021-09-16 16:46:38 -0500

    [Feature]: Hero

--- src/pages/HotelsScreen.jsx
+++ src/pages/HotelsScreen.jsx
@@ -1,11 +1,12 @@
-import React from "react";
+import React from 'react';
+import Hero from '../components/Header/Hero';
 
 const HotelsScreen = () => {
-  return (
-    <div>
-      <h1>Hotels</h1>
-    </div>
-  );
+	return (
+		<div>
+			<Hero id='hotels' title='Find your place to stay' />
+		</div>
+	);
 };
 
 export default HotelsScreen;
